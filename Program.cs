40dc21468d581968e8d16d549cf0f5bd79e53fb2
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-11 23:50:00 +0530

    Welcome to UC

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
 
         Console.WriteLine("Hello Fellow Learners!\nWelcome to Employee Wage Problem.\nEnter the index no. of the program you want to run.");
-        Console.WriteLine("1.Basic Code\n2.Refactored Code");
+        Console.WriteLine("1.Basic Code\n2.Refactored Code\n3.For Multiple Companies");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
@@ -18,6 +18,11 @@ internal class Program
             case 2:
                 EmployeeWageCalculator employeeWageCalculator1 = new EmployeeWageCalculator();
                 EmployeeWageCalculator.ComputeEmployeeWage();
+                break;
+            case 3:
+                EmployeeWageCalculator2 employeeWageCalculator2 = new EmployeeWageCalculator2();
+                EmployeeWageCalculator2.ComputeEmployeeWage2("Company A", 20, 100, 20); // Company A's parameters
+                EmployeeWageCalculator2.ComputeEmployeeWage2("Company B", 22, 110, 25); // Company B's parameters
                 break;
             default:
                 Console.WriteLine("Please Enter a Valid Index No.");
